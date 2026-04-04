3fbf4a9405db44904892d086602febbb0ee06c5f
teamsss-art <t72253427@gmail.com>
2026-04-04 11:33:01 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -333,7 +333,7 @@ cron.schedule("* * * * *", async () => {
     const [year, month, day] = indiaDate.split("-");
     const [hour, minute] = indiaTime.split(":");
 
-    if (!(Number(hour) === 11 && Number(minute) === 25)) {
+    if (!(Number(hour) === 11 && Number(minute) === 35)) {
       return;
     }
 
